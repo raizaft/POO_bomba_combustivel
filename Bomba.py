a4489a7c2fb4a67169ed963f845b327449d8dab0
Raiza <117646187+raizaft@users.noreply.github.com>
2023-03-10 13:40:36 -0300

    Update Bomba.py

--- Bomba.py
+++ Bomba.py
@@ -1,15 +1,25 @@
 class Bomba:
-    def __init__(self, tipo_combustivel:str, valor_litro:float=0):
+    def __init__(self, tipo_combustivel:str, valor_litro_alcool:float=0, valor_litro_gaso:float=0):
         self.tipo_combustivel = tipo_combustivel
-        self.valor_litro = valor_litro
+        self.valor_litro_alcool = valor_litro_alcool
+        self.valor_litro_gaso = valor_litro_gaso
+
 
     def abastecerPreco(self, valor):
-        litros = valor/self.valor_litro
-        print(f'Abasteceu {litros:.2f} litros por R${valor}.')
+        if self.tipo_combustivel == 'álcool':
+            litros = valor/self.valor_litro_alcool
+            print(f'Abasteceu {litros:.2f} litros de {self.tipo_combustivel} por R${valor}.')
+        else:
+            litros = valor/self.valor_litro_gaso
+            print(f'Abasteceu {litros:.2f} litros de {self.tipo_combustivel} por R${valor}.')
 
     def abastecerLitro(self, quantidade):
-        preco = quantidade * self.valor_litro
-        print(f'Abasteceu {quantidade:.2f} litros por R${preco:.2f}.')
+        if self.tipo_combustivel == 'álcool':
+            preco = quantidade * self.valor_litro_alcool
+            print(f'Abasteceu {quantidade:.2f} litros de {self.tipo_combustivel} por R${preco:.2f}.')
+        if self.tipo_combustivel == 'gasolina':
+            preco = quantidade * self.valor_litro_gaso
+            print(f'Abasteceu {quantidade:.2f} litros de {self.tipo_combustivel} por R${preco:.2f}.')
         
     def alterarValor(self):
         novoValor = float(input('Qual o valor do litro: '))
@@ -17,7 +27,4 @@ class Bomba:
 
     def alterarCombustivel(self, tipo_combustivel):
         self.tipo_combustivel = tipo_combustivel
-        print(f'Combustível: {tipo_combustivel}.')
-
-    def tipoCombustivel(self, tipo_combustivel):
-        self.tipo_combustivel = tipo_combustivel
+        print(f'Tipo de combustível alterado para {tipo_combustivel}.')
